72c8d36822f87df7d8025950e33952d783dbb5d9
Cyan <huetcyan@gmail.com>
2022-01-05 11:22:13 +0100

    CICD impossible à réaliser, annulation des tests Revert "Configuration testing + début écriture des tests"
    This reverts commit 5d0b9a04e05f46a05e9b036191c3c37002dbaf68.

--- test-app.py
+++
@@ -1,8 +0,0 @@
-import user.auth as auth
-import json
-
-
-def test_getuser():
-    response = auth.getuser()
-    data = json.loads(response.get_data(as_text=True))
-    assert data['result'] == 'nf'
